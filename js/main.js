9d66f4158456f19424d75664125e3c3248e6943d
TommasoBertossi <tommasobertossi@gmail.com>
2023-07-26 15:24:10 +0200

    add first ex js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let firstNumber = parseInt(prompt("Inserisci un numero"));
+let secondNumber = parseInt(prompt("Inserisci un numero"));
+let min;
+let max;
+
+// while (firstNumber || secondNumber == isNaN) {
+//     firstNumber = parseInt(prompt("Attenzione, stai sbagliando! Inserisci un numero"));
+//     secondNumber = parseInt(prompt("Attenzione, stai sbagliando! Inserisci un numero"));
+// }
+
+if (firstNumber > secondNumber) {
+    max = firstNumber;
+    min = secondNumber;
+} else {
+    max = secondNumber;
+    min = firstNumber;
+}
+
+let middleNumber = Math.random() * max + min;
+
+document.getElementById("number").innerHTML = middleNumber;
+
+
