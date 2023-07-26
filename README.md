b0dfadffcdd8bf973bdb37446749e13534e53fb4
TommasoBertossi <tommasobertossi@gmail.com>
2023-07-26 15:59:15 +0200

    add second ex readme

--- README.md
+++ README.md
@@ -10,3 +10,18 @@ Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale com
 - Chiedi all'utente un primo numero
 - Chiedo all'utente un secondo numero
 - Genero un n° casuale compreso tra i due
+
+## 2° Esercizio
+
+Dare la possibilità di inserire due parole. Verificare che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+### Logica
+
+- Chiedo all'utente una parola
+- Chiedo all'utente una seconda parola
+- **se** hanno la stessa lunghezza
+    - **allora** le stampo entrambe
+- **se invece** la prima parola è più lunga
+    - **allora** stampa la prima parola
+- **se invece** la seconda parola è più lunga
+    - **allora** stampa la seconda parola
--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
-let firstNumber = parseInt(prompt("Inserisci un numero"));
-let secondNumber = parseInt(prompt("Inserisci un numero"));
-let min;
-let max;
+// Esercizio 1
 
-// while (firstNumber || secondNumber == isNaN) {
-//     firstNumber = parseInt(prompt("Attenzione, stai sbagliando! Inserisci un numero"));
-//     secondNumber = parseInt(prompt("Attenzione, stai sbagliando! Inserisci un numero"));
+// let firstNumber = parseInt(prompt("Inserisci un numero"));
+// let secondNumber = parseInt(prompt("Inserisci un numero"));
+// let min;
+// let max;
+
+// // while (firstNumber || secondNumber == isNaN) {
+// //     firstNumber = parseInt(prompt("Attenzione, stai sbagliando! Inserisci un numero"));
+// //     secondNumber = parseInt(prompt("Attenzione, stai sbagliando! Inserisci un numero"));
+// // }
+
+// if (firstNumber > secondNumber) {
+//     max = firstNumber;
+//     min = secondNumber;
+// } else {
+//     max = secondNumber;
+//     min = firstNumber;
 // }
 
-if (firstNumber > secondNumber) {
-    max = firstNumber;
-    min = secondNumber;
-} else {
-    max = secondNumber;
-    min = firstNumber;
-}
+// let middleNumber = Math.random() * (max - min) + min;
 
-let middleNumber = Math.random() * max + min;
+// document.getElementById("number").innerHTML = middleNumber;
 
-document.getElementById("number").innerHTML = middleNumber;
 
+// Esercizio 2
 
